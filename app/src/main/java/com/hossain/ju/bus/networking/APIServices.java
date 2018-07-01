549b6768def195c9d96dac431654416a3b559242
mortuza <shakill1>
2018-07-01 11:24:19 +0600

    first

---
+++ app/src/main/java/com/hossain/ju/bus/networking/APIServices.java
@@ -0,0 +1,43 @@
+package com.hossain.ju.bus.networking;
+
+
+import com.hossain.ju.bus.model.Notice;
+import com.hossain.ju.bus.model.RouteSchedule;
+import com.hossain.ju.bus.model.route.Route;
+
+import java.util.HashMap;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by Tariqul.Islam on 8/1/17.
+ */
+
+public interface APIServices {
+
+    @POST("login")
+    @Headers({"app-type: consumer-app", "Content-Type: application/json"})
+    Call<ResponseBody> login(@Body HashMap<String, Object> body);
+
+
+    @GET("get-location/{id}")
+    @Headers({"app-type: consumer-app", "Content-Type: application/json"})
+    Call<ResponseWrapperArray<RouteSchedule>> getLocation(@Header("Authorization") String auth, @Path("id") int id);
+
+    @GET("get-all-routes")
+    @Headers({"app-type: consumer-app", "Content-Type: application/json"})
+    Call<ResponseWrapperArray<Route>> getAllRoutes(@Header("Authorization") String auth);
+
+    @GET("notice")
+    @Headers({"app-type: consumer-app", "Content-Type: application/json"})
+    Call<ResponseWrapperArray<Notice>> getAllNotification(@Header("Authorization") String auth);
+
+
+}
